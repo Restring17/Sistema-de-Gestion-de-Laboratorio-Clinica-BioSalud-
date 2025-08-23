dc1dc6d0266e97066479028f0bc31b96c8af0536
Restring17 <114541485+Restring17@users.noreply.github.com>
2025-08-22 21:05:35 -0500

    EstructuraInicialDistribuida

--- src/EjerciciosDePrincipios/src/LSP/Ejercicio1/main.java
+++ src/EjerciciosDePrincipios/src/LSP/Ejercicio1/main.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package LCP;
+package LSP.Ejercicio1;
 
 /**
  *
  * @author User
  */
-public class LCP {
+public class main {
     
 }
--- src/EjerciciosDePrincipios/src/OCP/Ejercicio2/main.java
+++ src/EjerciciosDePrincipios/src/OCP/Ejercicio2/main.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package OCP;
+package OCP.Ejercicio2;
 
 /**
  *
  * @author User
  */
-public class OCP {
+public class main {
     
 }
