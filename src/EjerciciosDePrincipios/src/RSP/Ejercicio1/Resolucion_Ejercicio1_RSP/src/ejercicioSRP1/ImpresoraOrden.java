a5c1026f5e67d31bf1d88e5fbb646ed3bfeb5c38
IsaacLCH12 <isaaclivaque123@gmail.com>
2025-08-22 22:13:10 -0500

    Add files via upload

---
+++ src/EjerciciosDePrincipios/src/RSP/Ejercicio1/Resolucion_Ejercicio1_RSP/src/ejercicioSRP1/ImpresoraOrden.java
@@ -0,0 +1,16 @@
+
+package ejercicioSRP1;
+
+public class ImpresoraOrden {
+    
+    public void imprimir(OrdenLaboratorio orden, Examen examen){
+        System.out.println("Orden #"+orden.getFechaOrden()+
+                           "\nFecha: "+ orden.getFechaOrden()+
+                           "\nPaciente: "+ orden.getPaciente()+
+                           "\nMedico: "+ orden.getIdMedico()+
+                           "\nExamen: "+ examen.getTipoExamen()+
+                            "("+examen.getObservaciones()+")"
+                );
+    }
+    
+}
---
+++ src/EjerciciosDePrincipios/src/RSP/Ejercicio1/Resolucion_Ejercicio1_RSP/src/ejercicioSRP1/OrdenLaboratorio.java
@@ -0,0 +1,35 @@
+
+package ejercicioSRP1;
+
+public class OrdenLaboratorio {
+    
+    private int idOrden;
+    private String fechaOrden;    
+    private String paciente;
+    private String idMedico;
+
+    public OrdenLaboratorio(int idOrden, String fechaOrden, String paciente, String idMedico) {
+        this.idOrden = idOrden;
+        this.fechaOrden = fechaOrden;
+        this.paciente = paciente;
+        this.idMedico = idMedico;
+    }
+
+    public int getIdOrden() {
+        return idOrden;
+    }
+
+    public String getFechaOrden() {
+        return fechaOrden;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public String getIdMedico() {
+        return idMedico;
+    }    
+    
+    
+}
