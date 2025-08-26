4b7cd56a7f5e7169f1c6b483ce7765ec1622afad
JVLeonardo <leonardodelgado.184@gmail.com>
2025-08-26 01:36:40 -0300

    Update RegistroTomaMuestra.java

--- src/EjerciciosDePrincipios/src/LSP/Ejercicio2/RegistroTomaMuestra.java
+++ src/EjerciciosDePrincipios/src/LSP/Ejercicio2/RegistroTomaMuestra.java
@@ -22,9 +22,10 @@ public class RegistroTomaMuestra {
         return "Muestra ID: " + idMuestra + "\n" +
            "Tipo de Examen: " + tipoExamen + "\n" +
            "Técnico: " + tecnico + "\n" +
-           "Fecha y Hora: " + fechaHora ;
-    }
+           "Fecha y Hora: " + fechaHora ;
+    }
 }
     
     
+
 
