43fd1a78e77962f2230bfa1e417503753c57ac7f
JVLeonardo <leonardodelgado.184@gmail.com>
2025-08-26 01:37:45 -0300

    Update main.java

--- src/EjerciciosDePrincipios/src/LSP/Ejercicio2/main.java
+++ src/EjerciciosDePrincipios/src/LSP/Ejercicio2/main.java
@@ -9,5 +9,15 @@ package LSP.Ejercicio2;
  * @author User
  */
 public class main {
-    
+    public static void main(String[] args) {
+        Paciente paciente = new Paciente("Carla","Celestino Morales","77335049","001");
+        Tecnico tecnico = new TecnicoLaboratorio("Diana Flores");
+        String tipoExamen = "Hemograma , Glucosa";
+
+       
+        RegistroTomaMuestra registro = new RegistroTomaMuestra(paciente, tecnico, tipoExamen);
+        System.out.println("Registro de toma de muestra guardado:");
+        System.out.println(registro);
+      
+    }
 }
